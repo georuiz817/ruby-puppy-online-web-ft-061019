03616f94cba00968625770763e405cabf14502af
George Ruiz <georuiz817@gmail.com>
2019-06-22 01:31:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,26 @@
-class Dog 
-attr_accessor :name
 
+class Dog
 
-@@all = []
+  attr_accessor :name
 
-def initialize(name)
-  @name = name
-  @@all << @all 
-end
+  @@all = []
 
-def self.all
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
     @@all.each do |dog|
       puts dog.name
-end
-end
-  
-def self.clear_all
-  @@all.clear 
-end
+    end
+  end
 
+  def self.clear_all
+    @@all.clear
+  end
+
+end
 
 
 
